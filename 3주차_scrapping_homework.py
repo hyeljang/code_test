e78fd8ee5491e9353d5432dfc50c67063e8b9138
hyeljang <hyeljang@naver.com>
2020-06-02 09:57:49 +0900

    지니뮤직_차트 스크래핑
    지니뮤직 차트 50 스크래핑 수행 -> 랭킹반영에 어려움이 존재 td.number 이후에 랭킹변화정보가 <span>으로 붙어 있는 부분을 어떻게 제거하고 랭킹숫자만 붙일 수 있을지 검토 필요.

---
+++ 3주차_scrapping_homework.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(
+    'https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1', headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+music = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+# music (tr들) 의 반복문을 돌리기
+for song in music:
+    # music 안에 a 가 있으면,
+    
+    a_tag = song.select_one('td.info > a.title.ellipsis')
+    b_tag = song.select_one('td.info > a.artist.ellipsis')
+
+
+    if a_tag is not None:
+        title = a_tag.text
+        if b_tag is not None:
+            singer = b_tag.text
+            rank = song.find('td.number')
+            print(rank,title.strip(),singer.strip())
+            #print(rank)
+
